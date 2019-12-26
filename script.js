f39c192b769c831a9ecd4d90ebafcaefb351f6f3
Иванов Александр <sabnack777@gmail.com>
2019-12-26 09:01:14 +0300

    Заготовка часов

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-var d = new Date();
-document.getElementById('clock').innerHTML = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = handler;
-xhr.open("GET", "http://worldtimeapi.org/api/timezone/America/Argentina/Salta");
-xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-xhr.send();
 
-function showClock() {
-    var obj = document.getElementById('clock');
-    var req = new XMLHttpRequest(); 
-    req.open("GET", "http://worldtimeapi.org/api/ip", true);
-    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    req.onreadystatechange = function () {
-if (req.readyState != 4 || req.status != 200) return; 
-console.log(req.responseText);
-obj.innerHTML = 'Время сервера ' + JSON.parse(req.responseText).datetime;
+
+function showClock()
+{
+var obj = document.getElementById('clock');
+var req;
+ 
+if (window.XMLHttpRequest)	req = new XMLHttpRequest();
+else if(window.ActiveXObject)
+{
+    try {
+        req = new ActiveXObject('Msxml2.XMLHTTP');  
+    } catch (e){}									
+    try {											
+        req = new ActiveXObject('Microsoft.XMLHTTP');
+    } catch (e){}
 };
-            }
-
-setInterval(showClock(), 3600);
+ 
+if (req) 
+{
+    req.onreadystatechange = function() 
+	{
+    	if (req.readyState == 4 && req.status == 200)  
+		{ 
+            tm = JSON.parse(req.responseText).datetime.substr(12,4);
+            
+            obj.innerHTML = 'Текущее время '+ tm; }        
+    };  
+    req.open("GET", 'http://worldtimeapi.org/api/ip', true);
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    req.send('clock=1');
+} 
+else alert("Браузер не поддерживает AJAX");
+}
+ 
+setInterval(showClock, 3600); 
